c1fd1db53b0635a6f2f0bf2aa4cb0b2f04c1bc0c
Armjkpn <43291765+Armjkpn@users.noreply.github.com>
2020-04-30 00:21:24 +0700

    27/04/63

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,77 +1,102 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import fire from '../firebase/Config';
+import "./Login.css"
+import firebase from 'firebase'
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-import auth from '../firebase';
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
 
-const Login = ({ setSession }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = async () => {
-    try {
-      const response = await auth.signInWithEmailAndPassword(
-        username,
-        password
-      );
-
-      const { user } = response;
-
-      setSession({
-        isLoggedIn: true,
-        currentUser: user
-      });
-    } catch (error) {
-      setSession({
-        isLoggedIn: false,
-        currentUser: null,
-        errorMessage: error.message
-      });
+    ],
+    callbacks: {
+      signInSuccess: () => false
     }
-  };
+  }
 
-  const handleRegister = async () => {
-    try {
-      const response = await auth.createUserWithEmailAndPassword(
-        username,
-        password
-      );
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-      const { user } = response;
-
-      setSession({
-        isLoggedIn: true,
-        currentUser: user
+  login(e) {
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).catch((error) => {
+        console.log(error);
       });
-    } catch (error) {
-      setSession({
-        isLoggedIn: false,
-        currentUser: null,
-        errorMessage: error.essage
-      });
-    }
-  };
+  }
 
-  const handleUsername = event => {
-    setUsername(event.target.value);
-  };
+  signup(e){
+    e.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).then((u)=>{console.log(u)})
+    .catch((error) => {
+        console.log(error);
+      })
+  }
 
-  const handlePassword = event => {
-    setPassword(event.target.value);
-  };
+  render() {
+    return (
+      <div class ="login">
+        <div class="login2">
+            <h1 >JSS Network Company</h1><hr />
+            <h2>Login</h2>
+          <input 
+              value={this.state.email}  
+              type="email" 
+              name="email"  
+              placeholder="Email address"
+              style={{ width: "60%"}}
+              class="form-control"
+              onChange={this.handleChange}
+          />
+      
+        <input 
+            value={this.state.password} 
+            type="password" 
+            name="password"  
+            placeholder="Password"
+            style={{ width: "60%", marginTop: "20px"}}
+            class="form-control"
+            onChange={this.handleChange}  
+        /><br /> 
+    
+      <button 
+          type="button" 
+          onClick={this.login}
+          class="form-control2"
+          style={{ width: "30%", marginTop: "15px" }}>
+            Login
+      </button><br />
 
-  return (
-    <div>
-      <input type="email" placeholder="Email" onChange={handleUsername} />
-      <input type="password" placeholder="Password" onChange={handlePassword} />
-
-      <button type="button" onClick={handleLogin}>
-        Login
-      </button>
-
-      <button type="button" onClick={handleRegister}>
-        Register
-      </button>
+      <button
+          type="button" 
+          onClick={this.signup}
+          class="form-control3" 
+          style={{ width: "30%", marginTop: "15px" }}>
+            Register
+      </button><br /><hr />
+      <h4 >Login with facebook :</h4>
+      <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+      </div>
     </div>
-  );
-};
+    );
+  } 
+}
 
 export default Login;
