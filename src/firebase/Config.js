7e0c423ec73a465f74f57387d099c7163c8082b5
Armjkpn <43291765+Armjkpn@users.noreply.github.com>
2020-04-30 00:27:26 +0700

    27/04/63

---
+++ src/firebase/Config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyBk7NRqeQPD4ORoaYztKd3aOC586tB4KbQ",
+  authDomain: "miniproject-88f54.firebaseapp.com",
+  databaseURL: "https://miniproject-88f54.firebaseio.com",
+  projectId: "miniproject-88f54",
+  storageBucket: "miniproject-88f54.appspot.com",
+  messagingSenderId: "730277569998",
+  appId: "1:730277569998:web:188004ab48670e8bd4779f",
+  measurementId: "G-6DBSJHZLL2"
+  };
+const Config = firebase.initializeApp(config);  
+export default Config;
