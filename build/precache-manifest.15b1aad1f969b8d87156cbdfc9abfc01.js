e75d517c1a644f2f03ef3995ad36bcb8b08eba92
Armjkpn <43291765+Armjkpn@users.noreply.github.com>
2020-04-30 00:05:09 +0700

    29/4/63

---
+++ build/precache-manifest.15b1aad1f969b8d87156cbdfc9abfc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258c6442c5dbdb96307c5a12bcba68c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901e63702644c73e462d",
+    "url": "/static/css/main.760e4c12.chunk.css"
+  },
+  {
+    "revision": "4215733bae35864fe860",
+    "url": "/static/js/2.03cc42d7.chunk.js"
+  },
+  {
+    "revision": "65b2bbc1257185d4add917f400c10925",
+    "url": "/static/js/2.03cc42d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901e63702644c73e462d",
+    "url": "/static/js/main.7d53b664.chunk.js"
+  },
+  {
+    "revision": "31e54225cbabdee0a905",
+    "url": "/static/js/runtime-main.af071126.js"
+  }
+]);
